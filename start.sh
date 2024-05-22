19691cbc68b5f62f3ce3e26faa4785ebcc540fcb
MarcoLor01 <marcolorenzini23@gmail.com>
2024-05-22 21:46:29 +0200

    Update start.sh file

--- start.sh
+++ start.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 
-# Imposta le variabili d'ambiente
-# Percorsi dei file Go
+
 server_files="servers"
 client_files="clients"
-operation_type="seq"
 
-# Ottieni il primo argomento da riga di comando come CONFIG
 CONFIG=$1
 
 if [ $CONFIG -eq 1 ]; then
     # Esecuzione dei server in nuovi terminali
     ENV_PATH="../.env"
-    gnome-terminal -- bash -c "cd $server_files && go run server1.go -m $operation_type"
-    gnome-terminal -- bash -c "cd $server_files && go run server2.go -m $operation_type"
-    gnome-terminal -- bash -c "cd $server_files && go run server3.go -m $operation_type"
-    gnome-terminal -- bash -c "cd $server_files && go run server4.go -m $operation_type"
-    gnome-terminal -- bash -c "cd $server_files && go run server5.go -m $operation_type"
+    gnome-terminal -- bash -c "cd $server_files && go run server1.go"
+    gnome-terminal -- bash -c "cd $server_files && go run server2.go"
+    gnome-terminal -- bash -c "cd $server_files && go run server3.go"
 
     # Esecuzione del client in un nuovo terminale
     gnome-terminal -- bash -c "cd $client_files && go run client.go"
